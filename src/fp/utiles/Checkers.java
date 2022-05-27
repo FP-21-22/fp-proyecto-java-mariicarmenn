09ce056b6583708291270c9ce849c3b8de952ad3
mariicarmenn <carmcg03@gmail.com>
2022-05-27 23:09:35 +0200

    -

--- src/fp/utiles/Checkers.java
+++ src/fp/utiles/Checkers.java
@@ -1,19 +1,7 @@
 package fp.utiles;
 
-/**
- * @author reinaqu
- * Clase de utilidad con métodos para facilitar la gestión de restricciones.
- */
 public class Checkers {
 
-	/**
-	 * @param textoRestriccion
-	 *     Mensaje que se incluirá en la excepción elevada
-	 * @param condicion
-	 *     Expresión booleana que ha de cumplirse para que no se eleve una excepción
-	 * @throws IllegalArgumentException si no se cumple la condición
-	 *  
-	 */
 	public static void check(String textoRestriccion, Boolean condicion) {
 		if (!condicion) {
 			throw new IllegalArgumentException(
@@ -25,10 +13,6 @@ public class Checkers {
 		}
 	}
 
-	/**
-	 * @param parametros Lista de parámetros para los que se quiere realizar una comprobación de nulidad.
-	 * @throws IllegalArgumentException si alguno de los parámetros es nulo.
-	 */
 	public static void checkNoNull(Object... parametros) {
 		for (int i = 0; i < parametros.length; i++) {
 			if (parametros[i] == null) {
